d289c596eba29669d0fa9c629ecdfcaee7184373
Artem <artem.bolohonov@gmail.com>
2021-02-22 03:36:00 +0700

    prog №1 review 1

--- main.c
+++ main.c
@@ -1,39 +1,44 @@
 #include<stdio.h>
 
-int main() {
-    int matrix[255][255];
+#define N 255
+
+int main()  {
+    int matrix[N][N];
     int a, b;
-    int srInStolb[255];
+    int srInStolb[N];
     scanf("%i %i", &a, &b);
-    int minInStr[255], maxInStolb[255];
-    for (int i = 0; i < b; i++) {
-        for (int j = 0; j < a; j++) {
+    int minInStr[N], maxInStolb[N];
+    for (int i = 0; i < 255; i++)  {
+        minInStr[i] = 0;
+        maxInStolb[i] = 0;
+    }
+    for (int i = 0; i < b; i++)  {
+        for (int j = 0; j < a; j++)  {
             scanf("%i", &matrix[i][j]);
-            if (matrix[i][j] < minInStr[i] && matrix[i][j] != 0 || minInStr[i] == 0) {
+            if (matrix[i][j] < minInStr[i] && matrix[i][j] != 0 || minInStr[i] == 0)  {
                 minInStr[i] = matrix[i][j];
             }
-            if (matrix[i][j] > maxInStolb[j]) {
+            if (matrix[i][j] > maxInStolb[j])  {
                 maxInStolb[j] = matrix[i][j];
             }
             srInStolb[j] += matrix[i][j];
         }
     }
-    for(int i = 0; i < a; i++){
+    for (int i = 0; i < a; i++)  {
         srInStolb[i] /= b;
     }
     int counter = 0;
-    for (int i = 0; i < a; i++) {
-        for (int j = 0; j < b; j++) {
-            //matrix[j][i] индексы поменялись местами
-            if(matrix[j][i] == minInStr[j] && matrix[j][i] == maxInStolb[i]){
-                counter+= 1;
-                matrix[j][i] = srInStolb[i];
+    for (int i = 0; i < b; i++)  {
+        for (int j = 0; j < a; j++)  {
+            if (matrix[i][j] == minInStr[i] && matrix[i][j] == maxInStolb[j])  {
+                counter++;
+                matrix[i][j] = srInStolb[j];
             }
         }
     }
     printf("%d\n", counter);
-    for (int i = 0; i < b; i++) {
-        for (int j = 0; j < a; j++) {
+    for (int i = 0; i < b; i++)  {
+        for (int j = 0; j < a; j++)  {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
